447aaf6a00f372825a7a7ae8cd10604f1ea642e2
Kamil Nabong <kamil@simple-merchant.com>
2017-05-10 19:45:52 +0000

    complete equipment request

--- src/app/requests/requests.equipment.controller.js
+++ src/app/requests/requests.equipment.controller.js
@@ -14,11 +14,13 @@ function EquipmentRequestsController($scope, $rootScope, $state, $timeout, $stat
 
   function activate() {
     $scope.mapping = {};
-    $scope.equipmentSelectionConfirmed = false;
+    $scope.data = {};
     $scope.selectedEquipment = null;
     $scope.loadingRequiredEquimentRequestData = true;
+    $scope.equipmentWizardCurrentStep = 0;
+
     $scope.newRequest = {
-      request_type: 'vehicle_request',
+      request_type: 'equipment_request',
       requester_id: $rootScope.authenticatedUser.id,
       request_status: 'pending',
     };
@@ -36,8 +38,8 @@ function EquipmentRequestsController($scope, $rootScope, $state, $timeout, $stat
     loadEquipment();
   }
 
-  $scope.addRequest = function (requestType) {
-
+  // ADD EQUIPMENT
+  function addEquipmentRequest() {
     populateEquipmentRequestData();
     $scope.addingRequest = true;
 
@@ -53,14 +55,69 @@ function EquipmentRequestsController($scope, $rootScope, $state, $timeout, $stat
     });
   };
 
+  // WIZARD CHECKS AND NEXT STEPS
+  $scope.equipmentRequestNextStep = function () {
+    if ($scope.equipmentWizardCurrentStep == 0) {
+      var proceed = $scope.selectedEquipment ? executeNextStep() :
+      ToastsService.showToast('error', 'Please select a vehicle before proceeding.');
+    } else if ($scope.equipmentWizardCurrentStep == 1) {
+      if ($scope.newRequest.request_cost && $scope.mapping.deliveryLocation.latitude) {
+        addEquipmentRequest();
+      } else {
+        ToastsService.showToast('error',
+        'Please enter both number of days and where you\'ll use the equipment');
+      }
+    }
+
+    function executeNextStep() {
+      WizardHandler.wizard('equipmentRequestWizard').next();
+      $scope.equipmentWizardCurrentStep =
+      WizardHandler.wizard('equipmentRequestWizard').currentStepNumber();
+      changeEquipmentRequestModalTitle();
+    }
+  };
+
+  // WIZARD PREVIOUS STEP
+  $scope.equipmentRequestPrevStep = function () {
+    WizardHandler.wizard('equipmentRequestWizard').previous();
+    $scope.equipmentWizardCurrentStep -= 1;
+    changeEquipmentRequestModalTitle();
+  };
+
+  // CHANGE REQUEST MODAL TITLE
+  function changeEquipmentRequestModalTitle() {
+    switch ($scope.equipmentWizardCurrentStep) {
+      case 0:
+        $scope.modalTitle = 'Search for your desired equipment';
+        break;
+      case 1:
+        $scope.modalTitle = 'Confirm and Submit Request';
+        break;
+      default:
+    }
+  };
+
+  // RESET WIZARD WHEN MODAL IS CLOESED
+  $scope.cancelEquipmentRequest = function () {
+    WizardHandler.wizard('equipmentRequestWizard').reset();
+    $rootScope.closeDialog();
+  };
+
   ////////////////////// HELPER FUNCTIONS ///////////////////////////
 
   function populateEquipmentRequestData() {
     $scope.newRequest.delivery_location_name = $scope.mapping.deliveryLocation.name;
     $scope.newRequest.delivery_location_latitude = $scope.mapping.deliveryLocation.latitude;
     $scope.newRequest.delivery_location_longitude = $scope.mapping.deliveryLocation.longitude;
+    $scope.newRequest.pickup_location_name = $scope.selectedEquipment.equipment_location_name;
+    $scope.newRequest.pickup_location_latitude =
+    $scope.selectedEquipment.equipment_location_latitude;
+    $scope.newRequest.pickup_location_longitude =
+    $scope.selectedEquipment.equipment_location_longitude;
+    $scope.newRequest.equipment_id = $scope.selectedEquipment.id;
   }
 
+  // LOAD EQUIPMENT
   function loadEquipment() {
     EquipmentService.getAllEquipment($scope.equipmentFilterParams)
     .then(function (response) {
@@ -69,13 +126,28 @@ function EquipmentRequestsController($scope, $rootScope, $state, $timeout, $stat
       $scope.filteringEquipment = false;
     })
     .catch(function (error) {
-      $scope.error = error.message;
       $scope.loadingRequiredEquimentRequestData = false;
       $scope.filteringEquipment = false;
       debugger;
     });
   }
 
+  // SELECT EQUIPMENT
+  $scope.selectEquipment = function (equipment) {
+    $scope.selectedEquipment = equipment;
+  };
+
+  // CALCULATE REQUEST COST
+  $scope.calculateRequestCost = function () {
+    if (isNaN($scope.data.numberOfDays)) {
+      ToastsService.showToast('error', 'That is not a valid number.');
+    }else {
+      $scope.newRequest.request_cost = $scope.data.numberOfDays *
+      $scope.selectedEquipment.equipment_price_per_day;
+    }
+  };
+
+  // FILTER EQUIPMENT
   $scope.filterEquipment = function (type) {
     switch (type) {
       case 'category':
